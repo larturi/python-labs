53193f8d1403c1dff8dffa5f528cc320a9761b14
Leandro Arturi <lea.arturi@gmail.com>
2022-07-26 20:18:45 -0300

    Between Two Sets

--- labs/between_two_sets.py
+++ labs/between_two_sets.py
@@ -2,7 +2,24 @@
 
 def getTotalX(a, b):
     
+    maximo = 101
+    resultados = []
     
-    pass
+    for i in range(1, maximo):
+        acumulador = 0
+        
+        for x in a:
+            acumulador = acumulador + i%x
+        
+        for y in b:
+            acumulador = acumulador + y%i   
+            
+        if acumulador == 0:
+            resultados.append(i)
+                                    
+    return len(resultados)
+        
+a = [1]
+b = [100]
 
-
+print(getTotalX(a, b))
